c05286512973e6fb52c66991fc0c90b65fe43f60
Aleksandr <sanchasyo@mail.ru>
2022-02-09 19:43:22 +0300

    try to end lab-1

---
+++ entity/Comment.php
@@ -0,0 +1,33 @@
+﻿<?php
+
+class Comment
+{
+    private User $user;
+    private string $comment;
+    private DateTime $createdAt;
+
+    public function __construct(User $user, string $comment)
+    {
+        $this->user = $user;
+        $this->comment = $comment;
+        $this->createdAt = $this->randomDate(new DateTime('2022-01-01'), new DateTime());
+    }
+
+    private function randomDate($start_date, $end_date)
+    {
+        $min = strtotime($start_date);
+        $max = strtotime($end_date);
+
+        $val = rand($min, $max);
+
+        return date('d.m.Y H:i:s', $val);
+    }
+    public function toString()
+    {
+        return  '<div style="display: flex;margin-top: 20px;">' . $this->comment . $this->user->toCommentString() . '</div>';
+    }
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+}
---
+++ index.php
@@ -0,0 +1,35 @@
+﻿<?php
+
+use Symfony\Component\Validator\Validation;
+
+
+
+$validator = Validation::createValidator();
+
+$user1 = new User(0, 'User1', 'user1@mail.ru', "123456");
+$user2 = new User(1, 'User2', 'user2@mail.ru', "1234");
+$users[] = $user1;
+$users[] = $user2;
+
+echo '<h2>Users</h2>';
+foreach ($users as $user) {
+    $errors = $validator->validate($user);
+    if (count($errors) > 0) {
+        $errorsString = (string) $errors;
+        echo $errorsString;
+    } else echo $user->toString();
+}
+
+$userComment = 'user №1 kek pem';
+$comment1 = new Comment($user1, $userComment);
+
+$userComment = 'user №1 ne ochen';
+$comment2 = new Comment($user1, $userComment);
+
+$comments[] = $comment1;
+$comments[] = $comment2;
+echo '<h2>Comments</h2>';
+$dateTime = new DateTime('2022-01-15');
+foreach ($comments as $comment) {
+    if ($comment->getCreatedAt() > $dateTime) echo $comment->toString();
+}
