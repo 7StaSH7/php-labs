fdccb30dbde6bc0dbe8db1c922e1e1bbc0ff6f53
Aleksandr <sanchasyo@mail.ru>
2022-04-05 22:11:17 +0300

    init lab-2

---
+++ lab-2/app/Models/Address.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'address',
+        'city',
+        'street',
+        'house',
+        'floor',
+        'flat',
+        'code',
+        'created_at',
+    ];
+
+
+    public function brand()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
